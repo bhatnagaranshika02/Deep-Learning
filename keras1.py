ba6c0e36e14a00015acb8b278dd624502aa7a547
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-09-03 15:11:01 +0530

    new

---
+++ keras1.py
@@ -0,0 +1,6 @@
+keras Model building teps:
+    1. specify arch (how many nods in each layer)
+    2. complie
+    3. fit
+    4. predict
+    
---
+++ keras2.py
@@ -0,0 +1,6 @@
+import numpy as np
+from keras.layers import Dense
+from keras.models import Sequential
+
+
+predictors = np.loadtxt('
