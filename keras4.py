bce01ebd94893f94044ee3ab4b8b58c410d5e35b
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-09-06 10:35:36 +0530

    new

---
+++ keras4.py
@@ -0,0 +1,13 @@
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+n_cols = predictors.shape[1]
+model = Sequential()
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+model.compile(optimizer='adam',loss='mean_squared_error')
+
+print("Loss function: " + model.loss)
